ab4326ffa7643a974ccb0bc8921af77edbecf26e
Dario L Garcia <dario.garcia@kfgodeldev.com>
2014-11-18 21:52:38 -0300

    Performance tests

--- src/test/java/ar/com/kfgodel/objectmapper/tests/ConversionPerformanceIT.java
+++ src/test/java/ar/com/kfgodel/objectmapper/tests/ConversionPerformanceIT.java
@@ -3,7 +3,18 @@ package ar.com.kfgodel.objectmapper.tests;
 import ar.com.dgarcia.javaspec.api.JavaSpec;
 import ar.com.dgarcia.javaspec.api.JavaSpecRunner;
 import ar.com.dgarcia.javaspec.api.TestContext;
+import ar.com.dgarcia.objectmapper.api.JacksonMapper;
+import ar.com.dgarcia.objectmapper.api.TypeMapper;
+import ar.com.kfgodel.objectmapper.tests.mapper.TypicalObjectMapper;
+import ar.com.kfgodel.objectmapper.tests.testObjects.TypicalObject;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This type verifies the minimum basic mapper
@@ -11,16 +22,29 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JavaSpecRunner.class)
 public class ConversionPerformanceIT extends JavaSpec<TestContext> {
+    public static Logger LOG = LoggerFactory.getLogger(ConversionPerformanceIT.class);
+
     @Override
     public void define() {
         describe("measuring conversion times", () -> {
 
             describe("from object to map", () -> {
-                it("native mapper",()->{
+                it("warm up",()->{
+                    TypicalObject typicalObject = TypicalObject.create();
+                    typicalObject.initializeRootTestData();
 
+                    TypicalObjectMapper testedMapper = TypicalObjectMapper.create();
+                    Map<String, Object> converted = null;
+                    for (int i = 0; i < 1_000_000; i++) {
+                        converted = testedMapper.toMap(typicalObject);
+                    }
+                    assertThat(converted).isEqualTo(TypicalObject.createRootTestMap());
+                });
+                it("native mapper",()->{
+                    runTestsOn(TypicalObjectMapper.create());
                 });
                 it("jackson mapper",()->{
-
+                    runTestsOn(JacksonMapper.create());
                 });
                 it("implementation mapper",()->{
 
@@ -40,5 +64,20 @@ public class ConversionPerformanceIT extends JavaSpec<TestContext> {
                 });
             });
         });
+    }
+
+    private void runTestsOn(TypeMapper testedMapper) {
+        TypicalObject typicalObject = TypicalObject.create();
+        typicalObject.initializeRootTestData();
+
+
+        long started = System.nanoTime();
+        Map<String, Object> converted = null;
+        for (int i = 0; i < 1_000_000; i++) {
+            converted = testedMapper.toMap(typicalObject);
+        }
+        long ended = System.nanoTime();
+        LOG.info("{}: {} ms", testedMapper.getClass().getSimpleName() , TimeUnit.NANOSECONDS.toMillis(ended - started));
+        assertThat(converted).isEqualTo(TypicalObject.createRootTestMap());
     }
 }
